8743164f2fb2bfc4240f8c1fc336b4e4bdfb28d7
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2024-05-25 21:57:40 +0300

    Updated!

--- generate-release.sh
+++ generate-release.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-[[ $# -eq 1 ]] || exit 1
+[[ $# -ge 1 ]] || exit 1
 
 set -o errexit -o nounset -o pipefail
 
 KEYSTORE=$PWD/../vanadium.keystore
 APKSIGNER=$PWD/third_party/android_sdk/public/build-tools/34.0.0/apksigner
+readonly APPS=(
+    TrichromeChrome
+    TrichromeLibrary
+    TrichromeWebView
+)
 
 read -p "Enter keystore passphrase: " -s keystore_pass
 echo
 
-cd "$1/Default/apks"
+for d in "$@"; do
+    cd "$d/Default/apks"
 
-rm -rf release
-mkdir release
-cd release
+    rm -rf release
+    mkdir release
+    cd release
 
-for app in TrichromeChrome TrichromeLibrary TrichromeWebView; do
-    $APKSIGNER sign --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias vanadium --in ../${app}6432.apk --out $app.apk <<< $keystore_pass
+    for app in ${APPS[@]}; do
+        $APKSIGNER sign --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias vanadium --in ../${app}*.apk --out $app.apk <<< $keystore_pass
+    done
+
+    cd ../../../..
 done
